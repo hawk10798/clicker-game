2e139cfe62ad97cd0c10fe73fd4eaa1d4b36cdc8
Harry Hawkesford <harry.hawkesford@sykescottages.co.uk>
2023-06-30 16:05:44 +0100

    Added upgrade counter

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -6,11 +6,22 @@ use Livewire\Component;
 
 class Counter extends Component
 {
-    public $count = 0;
+    public float $points = 0;
+    public float $increment = 1;
+    public float $cost = 10;
 
-    public function increment() 
+    public function increment()
     {
-        $this->count++;
+        $this->points += round($this->increment,1);
+    }
+
+    public function upgradeIncrement()
+    {
+        if(round($this->points,1) - round($this->cost, 1) >= 0.0) {
+            $this->increment = round($this->increment * 1.1, 1);
+            $this->points = round($this->points - $this->cost, 1);
+            $this->cost = round($this->cost * 1.5, 1);
+        }
     }
 
     public function render()
